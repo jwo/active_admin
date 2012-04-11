d44bbf7ad26a00dc6a276bbfe2b75f664f47e3d0
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2012-04-11 16:27:39 -0500

    Mongoid Collections are already an array at this point and do not respond to reorder

--- lib/active_admin/views/pages/index.rb
+++ lib/active_admin/views/pages/index.rb
@@ -40,7 +40,11 @@ module ActiveAdmin
           #
           # If we don't reorder, there may be some columns referenced in the order
           # clause that requires the original select.
-          collection.reorder("").limit(1).exists?
+          if collection.respond_to?(:reorder)
+            collection.reorder("").limit(1).exists?
+          else
+            collection.any?
+          end
         end
 
         def build_collection
